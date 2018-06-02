fd1de229e6015a3e693c39f542d453fe00cc7cca
Ayrton Marini <ayrton.marini@mercadolibre.com>
2018-06-02 20:03:50 -0300

    asdasd

--- grails-app/controllers/ahp/service/ResultsController.groovy
+++ grails-app/controllers/ahp/service/ResultsController.groovy
@@ -1,15 +1,13 @@
 package ahp.service
 
-import Jama.Matrix
-import ahp.utils.HierarchyTree
+
+
 import ahp.utils.PairwaiseMatrix
-import ahp.utils.VIPD
-import ahp.utils.ValueComparator
+
 
 
 class ResultsController {
     AHPToolService AHPToolService
-    MathService mathService
 
     def index() { }
 
@@ -31,37 +29,8 @@ class ResultsController {
                 criteriaMatrices.push(tempMatrix)
             }
         }
-
-//        TODO: esto seria del service AHP me parece. osea a partir de aca para abajo
-        HierarchyTree hierarchyTree = HierarchyTree.getHierarchyTree()
-        criteriaMatrices.each { criteriaMatrix ->
-            Map<String,Double> priorityVector =  mathService.getPriorityMap(criteriaMatrix)
-            priorityVector.each { key, value ->
-                key = key.replace(" ","")
-                def n = HierarchyTree.search(hierarchyTree.getRoot(),key)
-                if(n != null)
-                    n.getData().setLocalPriority(value)
-
-            }
-        }
-        HierarchyTree.setGlobalPriorities()
-        Map<String,Double> globalPriorityMap = HierarchyTree.getGlobalPriorityMap()
-        Matrix globalPriorityVector = mathService.transformToMatrix(globalPriorityMap)
-        alternativeMatrices = AHPToolService.orderAlternativeMatrices(alternativeMatrices)
-        Matrix matrixOfAlternativePriorities = new Matrix(AHPToolService.fillMatrixOfPriorityAlternativeVectors(alternativeMatrices))
-        Matrix resultsVector = matrixOfAlternativePriorities.transpose().times(globalPriorityVector)
-
-        Map<String,Double> resultsMap = [:]
-        Iterator listIterator = hierarchyTree.getAlternatives().iterator()
-        int cont = 0
-        while(listIterator.hasNext()){
-            resultsMap.put(listIterator.next(),resultsVector.get(cont,0))
-            cont++
-        }
-        ValueComparator bvc = new ValueComparator(resultsMap)
-        TreeMap<String,Double> sortedMap = new TreeMap<>(bvc)
-        sortedMap.putAll(resultsMap)
-        render(view:"seeTheResults",model: [resultsVector:sortedMap])
+        TreeMap<String,Double> resultsMap = AHPToolService.getTheResults(criteriaMatrices,alternativeMatrices)
+        render(view:"seeTheResults",model: [resultsVector:resultsMap])
 
     }
 }
--- grails-app/services/ahp/service/AHPToolService.groovy
+++ grails-app/services/ahp/service/AHPToolService.groovy
@@ -3,7 +3,7 @@ package ahp.service
 import Jama.Matrix
 import ahp.utils.HierarchyTree
 import ahp.utils.PairwaiseMatrix
-
+import ahp.utils.ValueComparator
 
 
 class AHPToolService {
@@ -17,6 +17,38 @@ class AHPToolService {
         sortedKeys.remove('action')
         Collections.sort(sortedKeys)
         return sortedKeys
+    }
+
+    def getTheResults(ArrayList<PairwaiseMatrix> criteriaMatrices,ArrayList<PairwaiseMatrix> alternativeMatrices){
+        HierarchyTree hierarchyTree = HierarchyTree.getHierarchyTree()
+        criteriaMatrices.each { criteriaMatrix ->
+            Map<String,Double> priorityVector =  mathService.getPriorityMap(criteriaMatrix)
+            priorityVector.each { key, value ->
+                key = key.replace(" ","")
+                def n = HierarchyTree.search(hierarchyTree.getRoot(),key)
+                if(n != null)
+                    n.getData().setLocalPriority(value)
+
+            }
+        }
+        HierarchyTree.setGlobalPriorities()
+        Map<String,Double> globalPriorityMap = HierarchyTree.getGlobalPriorityMap()
+        Matrix globalPriorityVector = mathService.transformToMatrix(globalPriorityMap)
+        alternativeMatrices = orderAlternativeMatrices(alternativeMatrices)
+        Matrix matrixOfAlternativePriorities = new Matrix(fillMatrixOfPriorityAlternativeVectors(alternativeMatrices))
+        Matrix resultsVector = matrixOfAlternativePriorities.transpose().times(globalPriorityVector)
+
+        Map<String,Double> resultsMap = [:]
+        Iterator listIterator = hierarchyTree.getAlternatives().iterator()
+        int cont = 0
+        while(listIterator.hasNext()){
+            resultsMap.put(listIterator.next(),resultsVector.get(cont,0))
+            cont++
+        }
+        ValueComparator bvc = new ValueComparator(resultsMap)
+        TreeMap<String,Double> sortedMap = new TreeMap<>(bvc)
+        sortedMap.putAll(resultsMap)
+        return sortedMap
     }
 
     def removeUselessKeys(Set keys){
